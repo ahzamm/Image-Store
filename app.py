f8d4af62fb3620c4e3615b3533ec15a79c1cc9a4
ahzam <ahzamahmed6@gmail.com>
2023-12-20 13:57:15 +0500

    Refactor image upload function to store additional metadata

--- app.py
+++ app.py
@@ -1,47 +1,32 @@
-from bson import ObjectId
 from flask import Flask, request
 from gridfs import GridFS
 from pymongo import MongoClient
 
 app = Flask(__name__)
 
-
 client = MongoClient('mongodb://dev_user:dev_password@localhost:27017/')
 db = client['image_store']
 fs = GridFS(db, collection="files")
 
-
-def store_image_in_db(file):
-    file_id = fs.put(file, filename='file.jpg')
-    print(f"File stored with id: {file_id}")
-
-
-
+def store_image_in_db(file, vector_id, filename):
+    fs.put(file, vector_id=vector_id, filename=filename)
+    print(f"File stored with vector_id: {vector_id} and filename: {filename}")
 
 @app.route('/photos', methods=['POST'])
 def upload_image():
-    if 'image' not in request.files:
-        return "No file part in the request", 400
+    if 'image' not in request.files or 'vector_id' not in request.form or 'filename' not in request.form:
+        return "Invalid request format", 400
+
     file = request.files['image']
-    if file.filename == '':
-        return "No selected file", 400
-    store_image_in_db(file)
+    vector_id = request.form['vector_id']
+    filename = request.form['filename']
+
+    if file.filename == '' or vector_id == '' or filename == '':
+        return "Incomplete data in the request", 400
+
+    store_image_in_db(file, vector_id, filename)
     return "Image successfully stored in MongoDB"
 
 if __name__ == '__main__':
     app.run(debug=True)
     client.close()
-
-
-
-
-
-
-def retrieve_image_from_db():
-    image_id = '6566512c3c8c69c1ccabc131'
-    file = fs.get(ObjectId(image_id))
-    output_path = './test_images/test_output_image1.jpg'
-    with open(output_path, 'wb') as output_file:
-        output_file.write(file.read())
-
-    print(f"Image retrieved and saved to: {output_path}")
